4d543211bddd503dd43ae7b4d49972968e941070
vaibhavmorye <moryevaibhav93@gmail.com>
2018-08-15 19:15:30 +0530

    heroku related changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/kitchen-frontend'));
 
 app.get('/*', function(req,res) {
     
